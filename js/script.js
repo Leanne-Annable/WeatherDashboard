fd03ea0ea7272d52c37788417c534330d2f626e0
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-01-27 09:38:32 +0000

    added basics pseudo code and split the API link to be able to input different values

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Create a weather dashboard with form inputs
+//     When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+//     When a user views the current weather conditions for that city they are presented with:
+//          The city name
+//          The date
+//          An icon representation of weather conditions
+//          The temperature
+//          The humidity
+//          The wind speed
+//     When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
+//          The date
+//          An icon representation of weather conditions
+//          The temperature
+//          The humidity
+//     When a user click on a city in the search history they are again presented with current and future conditions for that city
+
+// Base for API calls = ("https://api.openweathermap.org/data/2.5/forecast?lat=" + linkLatValue + "&lon=" + linkLonValue + "&appid=48028b3db5f1aefd0fc887212580e039")
