d033609d15b2c160ad62bbda18f893157d1854e7
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-01-27 22:09:09 +0000

    added ajax into function to get weather for the current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,55 +15,77 @@
 //          The humidity
 //     When a user click on a city in the search history they are again presented with current and future conditions for that city
 
-// Base for API calls = ("https://api.openweathermap.org/data/2.5/forecast?lat=" + linkLatValue + "&lon=" + linkLonValue + "&appid=48028b3db5f1aefd0fc887212580e039") <= my API key
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid=48028b3db5f1aefd0fc887212580e039    <= using city name in the search to get the lat and lon
 // https://openweathermap.org/forecast5#JSON - example
-// right after, use that to get the lat and lon information to get the weather
 // how to get the icon - https://openweathermap.org/weather-conditions
 
 
 // **created variables**
 var locationsArray = []; // Need to create an empty list to be able to store the list of cities searched
 var apiKey = "48028b3db5f1aefd0fc887212580e039"; // my API key
-var latVal = ""; // a variabble to store the latitude
-var lonVal = ""; // a variable to store the longitude
-var QueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latVal + "&lon=" + lonVal + "&appid=" + apiKey; // the query we can send in to get the result
+var cityInput = ""; // a variable to store the city
+var mainCard = $("#today"); // selects the main card to be able to put todays data in it
+var dailyCards = $("#five-forecast"); // selects anything held within the 5-day forecast section
 
+// calls the function to show previous searches as buttons
 renderSearchHistory()
 
 /* need to create an 'on click' event on the search button to:
     -   store the location searched into a list to display on screen
-        -   The list should be made of buttons or at least be clickable - DONE
-        -   The list should prepend so that the latest search is at the top of the list - DONE
+        -   The list should be made of buttons or at least be clickable - *DONE*
+        -   The list should prepend so that the latest search is at the top of the list - *DONE*
         -   The list should also be re-clickable to retrieve past data again
     -   retrieve the information from the website about that location via ajax
         -   This should be called using the lat and lon - explore the weather api app to find link possibilities
-    -   store that info into local storage so it will be used again in future
+    -   store that info into local storage so it will be used again in future - *DONE*
         -   This is what will be called again by clicking the list buttons/name
     -   present the retrieved information on screen in the form of current weather and also a five day forecast.
         -   Need to have the current data filling the top section and include location, date, font-awesome icon and stats
             -   Need to link current date via moment
         -   The 5 day forecast needs to be five individual boxes with background color, date, icon and stats
-            -   dates linked via moment using the moment.js date and .add(x, 'days') <= https://momentjs.com/docs/#/manipulating/add/
 */
 
-function renderSearchHistory(){
+// function for getting weather data
+function getWeather() {
+    // empty any current values for todays weather and the forecast
+    mainCard.empty()
+    dailyCards.empty()
+    // do an ajax call to the website
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey + "&units=metric",
+        method: "GET"
+    }).then(function (response) {
+        // use moment to call the date
+        var date = moment.unix(response.dt + response.timezone).format("DD/MM/YYYY");
+        var name = $("<h3>").text(cityInput + " (" + date + ")");
+        var iconURL = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+        var temp = $("<p>").text("Temp: " + response.main.temp + "°C");
+        var windSpeed = $("<p>").text("Wind: " +response.wind.speed + "mtr/sec");
+        var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");      
+        mainCard.append(name, iconURL, temp, windSpeed, humidity)
+
+        })
+
+    }
+
+
+
+// function for rendering the search history from the local storage
+function renderSearchHistory() {
     // get any store info from local storage
     var previousSearches = JSON.parse(localStorage.getItem("SearchHistory"));
     // if there is something previously saved, the locations array should equal the stored data
-    if (previousSearches !== null){
+    if (previousSearches !== null) {
         locationsArray = previousSearches
     }
     renderButtons()
 }
 
-
 //function for rendering the buttons to screen given the cities searched/search history
-function renderButtons(){
+function renderButtons() {
     // delete the buttons prior to adding new cities to stop repeated buttons
     $("#city-list").empty();
     //loop through the array of cities
-    for (var i=0; i< locationsArray.length; i++){
+    for (var i = 0; i < locationsArray.length; i++) {
         // generate a button for each entry
         var a = $("<button>");
         // Adding a class of city to our button
@@ -78,15 +100,16 @@ function renderButtons(){
 }
 
 // **click-events**
-$("#search-button").on("click", function(event){
+$("#search-button").on("click", function (event) {
     event.preventDefault();
 
     // grab the input from the text box
-    var cityInput = $("#search-input").val();
-
+    cityInput = $("#search-input").val();
+    // get the data from the weather site using a created getWeather function
+    getWeather();
     // check if the cityInput is already in the locationsArray
     var checkArray = locationsArray.includes(cityInput);
-    if (checkArray === true){
+    if (checkArray === true) {
         // alert the user and end the search
         alert("That City is already in your search History")
         return
@@ -97,16 +120,21 @@ $("#search-button").on("click", function(event){
         localStorage.setItem("SearchHistory", JSON.stringify(locationsArray));
     };
 
-    console.log(locationsArray) // check to see if it works - it does XD
+    //console.log(locationsArray) // check to see if it works - it does XD
 
     renderButtons() // call a function to render the buttons of the cities selected
 })
 
+//click event for the buttons in the search history
+$(".city").on("click", function (event) {
+    event.preventDefault();
+
+})
 
 // ** Style changes/notes **
 // need to colour the header in an ombre of blue and have white text
-// search text box needs to fill aside width and have curved edges
-// search button to be blue with curved edges and also fill aside width
-// location list/buttons need to be light grey with curved edges and fill the aside width
+// search text box needs to fill aside width and have curved edges - *DONE*
+// search button to be blue with curved edges and also fill aside width - *DONE*
+// location list/buttons need to be light grey with curved edges and fill the aside width - *DONE*
 // 5-day forecast boxes need a background colour and to have a margin/padding round them
 
